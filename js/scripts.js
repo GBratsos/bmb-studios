8b07ed5eecd2eca62de000f23d98d48add66af53
Smooth Operator <smoothoperator@linux-m9dg.site>
2014-08-28 20:19:34 +0300

    Site upgrade

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	$('.wp5').waypoint(function() {
+		$('.wp5').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+
+});
+
+/***************** Smooth Scrolling ******************/
+
+$(function() {
+
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 2000);
+				return false;
+			}
+		}
+	});
+
+});
